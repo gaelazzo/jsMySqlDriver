4589c218edab90d098016b670f9d3a9168c04b73
Gaetano Lazzo <gaetano.lazzo@gmail.com>
2016-04-14 12:50:17 +0000

    improved query packets threading

--- src/jsMySqlDriver.js
+++ src/jsMySqlDriver.js
@@ -474,6 +474,10 @@ Connection.prototype.queryPackets = function (query, raw, packSize) {
         lastMeta,
         currentSet = -1,
         callback = function (data) {
+            if (data.resolve) {
+                def.resolve();
+                return;
+            }
             if (data.meta) {
                 currentSet += 1;
             }
@@ -487,15 +491,21 @@ Connection.prototype.queryPackets = function (query, raw, packSize) {
                     def.notify({rows: simpleObjectify(lastMeta, data.rows), set: currentSet});
                 }
             }
-        },
-        edgeQuery = edge.func(this.sqlCompiler, _.assign({source: query, callback: callback, packetSize: packetSize},
-            this.getDbConn()));
-    edgeQuery({}, function (error) {
-        if (error) {
-            def.reject(error + ' running ' + query);
-            return;
-        }
-        def.resolve();
+        };
+    var that = this;
+    process.nextTick(function() {
+        console.log('nextTick');
+        var edgeQuery = edge.func(that.sqlCompiler, _.assign({source: query, callback: callback, packetSize: packetSize},
+            that.getDbConn()));
+
+        edgeQuery({}, function (error) {
+            if (error) {
+                def.reject(error + ' running ' + query);
+                return;
+            }
+            //console.log('resolving '+query);
+            //def.resolve();
+        });
     });
     return def.promise();
 };
